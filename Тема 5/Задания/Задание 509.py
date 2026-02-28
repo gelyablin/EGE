270afdf712159eb38cc2077156a31d0f83a76fa4
Андрей Боев <a.boev.it@gmail.com>
2026-02-28 14:32:07 +0300

    NEW: Темы: 5, 6

---
+++ Тема 5/Задания/Задание 509.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 509, answer, '8f14e45fceea167a5a36dedd4bea2543'))
