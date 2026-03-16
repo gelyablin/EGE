426c168d503705db26a8f603ab7c7b9746c8f253
Андрей Боев <a.boev.it@gmail.com>
2026-03-16 13:35:15 +0300

    UPD: исключение разборов

--- Тема 6/Задания/+Задание 6.py
+++ Тема 6/Задания/+Задание 6.py
@@ -30,9 +30,3 @@ exitonclick()
 
 
 answer = 185
-
-#
-
-from tests.conftest import result_register
-if answer is not Ellipsis:
-    print(result_register(6, 6, answer, 'eecca5b6365d9607ee5a9d336962c534'))
